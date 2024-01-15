c1d3562134217f39ea98c7beecf53a9dbdbe6b6b
machunli <machunli20232023@outlook.com>
2024-01-15 12:38:23 +0800

    最后尝试之枝干提交02

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("kkkkkkkkkkkkk");
+        System.out.println("kkkkkkk哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈kkkkkk");
     }
 }
