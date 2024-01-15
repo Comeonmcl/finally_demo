e4646904c1faa5eb5c2b70a4f69d2692548657e4
machunli <machunli20232023@outlook.com>
2024-01-15 12:47:38 +0800

    最后尝试之分支提交03

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk");
+        System.out.println("kkkkkkk哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈kkkkkk");
     }
 }
